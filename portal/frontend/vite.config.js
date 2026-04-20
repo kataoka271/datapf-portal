1600d48fa03942c46a9cb10329831ee40270c245
Hiroaki Kataoka <k_hir@hotmail.com>
2026-04-21 01:23:33 +0900

    fix: package options

--- portal/frontend/vite.config.js
+++ portal/frontend/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: path.resolve(__dirname, '../backend/static'),
+        emptyOutDir: true,
+        assetsDir: 'assets'
+    },
     resolve: {
         alias: { "@": path.resolve(__dirname, "./src") },
     },
